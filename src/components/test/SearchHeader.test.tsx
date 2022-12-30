c3d3e75fc7ed2d7781cdc21ffce4b7916cdbddd0
Jongco <narira0922@gmail.com>
2022-12-30 20:28:37 +0900

    test: searchHeader에 검색 시 해당 페이지로 잘 이동하는지 확인

--- src/components/test/SearchHeader.test.tsx
+++ src/components/test/SearchHeader.test.tsx
@@ -2,6 +2,7 @@ import SearchHeader from "../SearchHeader";
 import userEvent from "@testing-library/user-event";
 import { create } from "react-test-renderer";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
 
 describe("search header", () => {
   it("renders correctly", () => {
@@ -14,5 +15,38 @@ describe("search header", () => {
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
-  it("black pink 입력 시 화면에 출력", async () => {});
+
+  it("'blackpink'로 이동 시 input에 출력", async () => {
+    render(
+      <MemoryRouter initialEntries={["/videos/blackpink"]}>
+        <Routes>
+          <Route path='/videos/:keyword' element={<SearchHeader />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByDisplayValue("blackpink")).toBeInTheDocument();
+  });
+
+  it("blackpink 검색 시 화면에 출력", () => {
+    const searchKeyword = "hello-world";
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path='/' element={<SearchHeader />} />
+          <Route
+            path='/videos/:keyword'
+            element={<div>{`Search result for ${searchKeyword}`}</div>}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+    const searchButton = screen.getByRole("button");
+    const searchInput = screen.getByRole("textbox");
+
+    userEvent.type(searchInput, searchKeyword);
+    userEvent.click(searchButton);
+
+    expect(screen.getByText(`Search result for ${searchKeyword}`)).toBeInTheDocument();
+  });
 });
