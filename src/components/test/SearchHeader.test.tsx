7654b3bddf8277db4f94129e039cc58923d47821
Jongco <narira0922@gmail.com>
2022-12-30 19:59:00 +0900

    test: make a snapshots

--- src/components/test/SearchHeader.test.tsx
+++ src/components/test/SearchHeader.test.tsx
@@ -1,11 +1,18 @@
 import SearchHeader from "../SearchHeader";
 import userEvent from "@testing-library/user-event";
 import { create } from "react-test-renderer";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 describe("search header", () => {
   it("renders correctly", () => {
-    const component = create(<MemoryRouter></MemoryRouter>);
+    const component = create(
+      <MemoryRouter>
+        <Routes>
+          <Route path='/' element={<SearchHeader />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
   });
   it("black pink 입력 시 화면에 출력", async () => {});
 });
